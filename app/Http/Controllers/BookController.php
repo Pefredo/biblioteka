b9140695fc116ec01a63fddd6089d6f96c30ff08
GosiaZ <s11483@pjwstk.edu.pl>
2020-01-13 13:58:34 +0100

    Query builder

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Book;
 use App\Models\Isbn;
+use Illuminate\Support\Facades\DB;
 
 
 class BookController extends Controller
@@ -107,5 +108,24 @@ class BookController extends Controller
 		
 		return redirect('books');
     }
+	
+	public function cheapest(Book $book)
+	{
+		$booksList = DB::table('books')->orderBy('price', 'asc')->limit(3)->get();
+		return view('books/list',['booksList'=> $booksList]);
+	}
+	
+	public function longest(Book $book)
+	{
+		$booksList = DB::table('books')->orderBy('pages', 'desc')->limit(3)->get();
+		return view('books/list',['booksList'=> $booksList]);
+	}
+	
+	public function search(Request $request, Book $book)
+	{
+		$q = $request->input('q', "");
+		$booksList = DB::table('books')->where('name', 'like', "%" . $q . "%")->get();
+		return view('books/list',['booksList'=> $booksList]);
+	}
 
 }
